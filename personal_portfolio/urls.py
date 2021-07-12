3ed3843e03ea28b497ab9c99fea894cc3f45daa8
PraseedaSaripalle <73073142+PraseedaSaripalle@users.noreply.github.com>
2021-07-13 00:33:11 +0530

    Update urls.py

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("projects.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
