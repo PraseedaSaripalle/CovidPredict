660486eb4251bb199de96f8345fbe876913c03f3
PraseedaSaripalle <73073142+PraseedaSaripalle@users.noreply.github.com>
2021-07-13 07:00:01 +0530

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,5 @@ from projects import views
 urlpatterns = [
     path("projects/", views.project_index, name='project_index'),
     path("covid/", views.home_view, name='home_view'),
-    path("corona_view/", views.corona_view, name='corona_view'),
+    path("", views.corona_view, name='corona_view'),
 ]
